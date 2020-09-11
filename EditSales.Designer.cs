d82b506636fff2326d7f0a028a498d96b4f45d0d
jimuskin <jimuskin1234567890@gmail.com>
2020-09-11 20:07:00 +1000

    Prettyfied the edit screen a little bit.

--- EditSales.Designer.cs
+++ EditSales.Designer.cs
@@ -45,8 +45,8 @@
             // 
             // btnExit
             // 
-            this.btnExit.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.btnExit.Location = new System.Drawing.Point(662, 404);
+            this.btnExit.Font = new System.Drawing.Font("Comic Sans MS", 13F, System.Drawing.FontStyle.Bold);
+            this.btnExit.Location = new System.Drawing.Point(75, 400);
             this.btnExit.Name = "btnExit";
             this.btnExit.Size = new System.Drawing.Size(137, 46);
             this.btnExit.TabIndex = 7;
@@ -58,7 +58,7 @@
             // 
             this.cmbSalesID.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
             this.cmbSalesID.FormattingEnabled = true;
-            this.cmbSalesID.Location = new System.Drawing.Point(12, 41);
+            this.cmbSalesID.Location = new System.Drawing.Point(84, 39);
             this.cmbSalesID.Name = "cmbSalesID";
             this.cmbSalesID.Size = new System.Drawing.Size(116, 21);
             this.cmbSalesID.TabIndex = 8;
@@ -67,66 +67,72 @@
             // lblSalesID
             // 
             this.lblSalesID.AutoSize = true;
-            this.lblSalesID.Location = new System.Drawing.Point(13, 13);
+            this.lblSalesID.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSalesID.Location = new System.Drawing.Point(98, 13);
             this.lblSalesID.Name = "lblSalesID";
-            this.lblSalesID.Size = new System.Drawing.Size(50, 13);
+            this.lblSalesID.Size = new System.Drawing.Size(80, 23);
             this.lblSalesID.TabIndex = 9;
             this.lblSalesID.Text = "Sales ID:";
             // 
             // lblProductID
             // 
             this.lblProductID.AutoSize = true;
-            this.lblProductID.Location = new System.Drawing.Point(13, 94);
+            this.lblProductID.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProductID.Location = new System.Drawing.Point(88, 85);
             this.lblProductID.Name = "lblProductID";
-            this.lblProductID.Size = new System.Drawing.Size(58, 13);
+            this.lblProductID.Size = new System.Drawing.Size(92, 23);
             this.lblProductID.TabIndex = 10;
             this.lblProductID.Text = "Product ID";
             // 
             // lblQuantity
             // 
             this.lblQuantity.AutoSize = true;
-            this.lblQuantity.Location = new System.Drawing.Point(13, 159);
+            this.lblQuantity.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblQuantity.Location = new System.Drawing.Point(96, 166);
             this.lblQuantity.Name = "lblQuantity";
-            this.lblQuantity.Size = new System.Drawing.Size(46, 13);
+            this.lblQuantity.Size = new System.Drawing.Size(76, 23);
             this.lblQuantity.TabIndex = 11;
             this.lblQuantity.Text = "Quantity";
             // 
             // lblDate
             // 
             this.lblDate.AutoSize = true;
-            this.lblDate.Location = new System.Drawing.Point(9, 229);
+            this.lblDate.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblDate.Location = new System.Drawing.Point(114, 253);
             this.lblDate.Name = "lblDate";
-            this.lblDate.Size = new System.Drawing.Size(30, 13);
+            this.lblDate.Size = new System.Drawing.Size(45, 23);
             this.lblDate.TabIndex = 12;
             this.lblDate.Text = "Date";
             // 
             // tpDate
             // 
             this.tpDate.CalendarFont = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.tpDate.Location = new System.Drawing.Point(12, 245);
+            this.tpDate.Font = new System.Drawing.Font("Comic Sans MS", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.tpDate.Location = new System.Drawing.Point(44, 279);
             this.tpDate.Name = "tpDate";
-            this.tpDate.Size = new System.Drawing.Size(202, 20);
+            this.tpDate.Size = new System.Drawing.Size(202, 23);
             this.tpDate.TabIndex = 13;
             // 
             // txtProductID
             // 
-            this.txtProductID.Location = new System.Drawing.Point(12, 111);
+            this.txtProductID.Location = new System.Drawing.Point(84, 111);
             this.txtProductID.Name = "txtProductID";
             this.txtProductID.Size = new System.Drawing.Size(100, 20);
             this.txtProductID.TabIndex = 14;
             // 
             // txtQuantity
             // 
-            this.txtQuantity.Location = new System.Drawing.Point(12, 176);
+            this.txtQuantity.Location = new System.Drawing.Point(84, 192);
             this.txtQuantity.Name = "txtQuantity";
             this.txtQuantity.Size = new System.Drawing.Size(100, 20);
             this.txtQuantity.TabIndex = 15;
             // 
             // btnEdit
             // 
-            this.btnEdit.Location = new System.Drawing.Point(12, 308);
+            this.btnEdit.Font = new System.Drawing.Font("Comic Sans MS", 13F, System.Drawing.FontStyle.Bold);
+            this.btnEdit.Location = new System.Drawing.Point(75, 350);
             this.btnEdit.Name = "btnEdit";
-            this.btnEdit.Size = new System.Drawing.Size(75, 23);
+            this.btnEdit.Size = new System.Drawing.Size(137, 41);
             this.btnEdit.TabIndex = 16;
             this.btnEdit.Text = "Edit Record";
             this.btnEdit.UseVisualStyleBackColor = true;
@@ -135,37 +141,41 @@
             // lblProductIDError
             // 
             this.lblProductIDError.AutoSize = true;
+            this.lblProductIDError.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblProductIDError.ForeColor = System.Drawing.SystemColors.Desktop;
-            this.lblProductIDError.Location = new System.Drawing.Point(13, 134);
+            this.lblProductIDError.Location = new System.Drawing.Point(63, 134);
             this.lblProductIDError.Name = "lblProductIDError";
-            this.lblProductIDError.Size = new System.Drawing.Size(92, 13);
+            this.lblProductIDError.Size = new System.Drawing.Size(149, 23);
             this.lblProductIDError.TabIndex = 17;
             this.lblProductIDError.Text = "Invalid Product ID";
             // 
             // lblQuantityError
             // 
             this.lblQuantityError.AutoSize = true;
+            this.lblQuantityError.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.lblQuantityError.ForeColor = System.Drawing.SystemColors.Desktop;
-            this.lblQuantityError.Location = new System.Drawing.Point(12, 204);
+            this.lblQuantityError.Location = new System.Drawing.Point(71, 226);
             this.lblQuantityError.Name = "lblQuantityError";
-            this.lblQuantityError.Size = new System.Drawing.Size(80, 13);
+            this.lblQuantityError.Size = new System.Drawing.Size(133, 23);
             this.lblQuantityError.TabIndex = 18;
             this.lblQuantityError.Text = "Invalid Quantity";
             // 
             // lblRecordResult
             // 
+            this.lblRecordResult.Anchor = System.Windows.Forms.AnchorStyles.Left;
             this.lblRecordResult.AutoSize = true;
-            this.lblRecordResult.Location = new System.Drawing.Point(13, 283);
+            this.lblRecordResult.Font = new System.Drawing.Font("Comic Sans MS", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblRecordResult.Location = new System.Drawing.Point(30, 320);
             this.lblRecordResult.Name = "lblRecordResult";
-            this.lblRecordResult.Size = new System.Drawing.Size(51, 13);
+            this.lblRecordResult.Size = new System.Drawing.Size(222, 23);
             this.lblRecordResult.TabIndex = 19;
-            this.lblRecordResult.Text = "Success!";
+            this.lblRecordResult.Text = "Success! X records affected.";
             // 
             // EditSales
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(284, 461);
             this.Controls.Add(this.lblRecordResult);
             this.Controls.Add(this.lblQuantityError);
             this.Controls.Add(this.lblProductIDError);
