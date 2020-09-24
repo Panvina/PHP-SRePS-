4dbdeabe93e54872680d6b2fb3a0c7dafb50a86e
102090870 <62122395+102090870@users.noreply.github.com>
2020-09-24 14:08:17 +1000

    Added Yearly Sales

--- DisplayYearlySales.Designer.cs
+++ DisplayYearlySales.Designer.cs
@@ -29,6 +29,12 @@
         private void InitializeComponent()
         {
             this.btnBack = new System.Windows.Forms.Button();
+            this.lblYear = new System.Windows.Forms.Label();
+            this.txtYear = new System.Windows.Forms.TextBox();
+            this.lblInvalidYear = new System.Windows.Forms.Label();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.dgvDisplay = new System.Windows.Forms.DataGridView();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDisplay)).BeginInit();
             this.SuspendLayout();
             // 
             // btnBack
@@ -41,20 +47,84 @@
             this.btnBack.UseVisualStyleBackColor = true;
             this.btnBack.Click += new System.EventHandler(this.btnBack_Click);
             // 
+            // lblYear
+            // 
+            this.lblYear.AutoSize = true;
+            this.lblYear.Font = new System.Drawing.Font("Comic Sans MS", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblYear.Location = new System.Drawing.Point(299, 31);
+            this.lblYear.Name = "lblYear";
+            this.lblYear.Size = new System.Drawing.Size(82, 19);
+            this.lblYear.TabIndex = 11;
+            this.lblYear.Text = "Enter Year";
+            // 
+            // txtYear
+            // 
+            this.txtYear.Location = new System.Drawing.Point(289, 68);
+            this.txtYear.Name = "txtYear";
+            this.txtYear.Size = new System.Drawing.Size(102, 20);
+            this.txtYear.TabIndex = 18;
+            // 
+            // lblInvalidYear
+            // 
+            this.lblInvalidYear.AutoSize = true;
+            this.lblInvalidYear.Font = new System.Drawing.Font("Comic Sans MS", 8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblInvalidYear.ForeColor = System.Drawing.Color.Red;
+            this.lblInvalidYear.Location = new System.Drawing.Point(215, 102);
+            this.lblInvalidYear.Name = "lblInvalidYear";
+            this.lblInvalidYear.Size = new System.Drawing.Size(247, 15);
+            this.lblInvalidYear.TabIndex = 20;
+            this.lblInvalidYear.Text = "Invalid Year: Needs to be 4 digits (2020,1999)";
+            this.lblInvalidYear.Visible = false;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Comic Sans MS", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(289, 120);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(88, 34);
+            this.btnSearch.TabIndex = 21;
+            this.btnSearch.Text = "Search";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // dgvDisplay
+            // 
+            this.dgvDisplay.AllowUserToAddRows = false;
+            this.dgvDisplay.AllowUserToDeleteRows = false;
+            this.dgvDisplay.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dgvDisplay.Location = new System.Drawing.Point(136, 157);
+            this.dgvDisplay.Name = "dgvDisplay";
+            this.dgvDisplay.ReadOnly = true;
+            this.dgvDisplay.RowHeadersWidth = 62;
+            this.dgvDisplay.Size = new System.Drawing.Size(440, 281);
+            this.dgvDisplay.TabIndex = 22;
+            // 
             // DisplayYearlySales
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.dgvDisplay);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.lblInvalidYear);
+            this.Controls.Add(this.txtYear);
+            this.Controls.Add(this.lblYear);
             this.Controls.Add(this.btnBack);
             this.Name = "DisplayYearlySales";
             this.Text = "DisplayYearlySales";
+            ((System.ComponentModel.ISupportInitialize)(this.dgvDisplay)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
 
         private System.Windows.Forms.Button btnBack;
+        private System.Windows.Forms.Label lblYear;
+        private System.Windows.Forms.TextBox txtYear;
+        private System.Windows.Forms.Label lblInvalidYear;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.DataGridView dgvDisplay;
     }
 }
--- DisplayYearlySales.cs
+++ DisplayYearlySales.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.Data.SqlClient;
 using System.Drawing;
 using System.Linq;
 using System.Text;
@@ -22,6 +23,42 @@ namespace PHP
             this.Hide();
             Homepage home = new Homepage();
             home.Show();
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            int Year;
+
+            lblInvalidYear.Visible = true;
+
+            if (int.TryParse(txtYear.Text, out Year))
+            {
+                if ((Year >= 1753) && (Year <= 9999))
+                {
+                    lblInvalidYear.Visible = false;
+                }
+            }
+
+            if ((lblInvalidYear.Visible == true)) 
+            {
+                return;
+            }
+
+            SqlCommand cmd = new SqlCommand();
+            cmd.Connection = Login.con;
+            cmd.CommandText = $"SELECT * FROM SALES WHERE YEAR(DATE) = {txtYear.Text}";
+
+            BindingSource bs = new BindingSource();
+            DataSet ds = new DataSet();                     // create new dataset
+
+            SqlDataAdapter adapter = new SqlDataAdapter();
+
+            adapter.SelectCommand = cmd;        // stores records from command
+            adapter.Fill(ds);                   // fills dataset with records
+
+            bs.DataSource = ds.Tables[0];       // set datasource of new BindingSource
+
+            dgvDisplay.DataSource = bs;           // set datasource of data grid view
         }
     }
 }
